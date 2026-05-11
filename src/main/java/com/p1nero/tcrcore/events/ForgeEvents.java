9feef2ac6377c94b296497709bc9150b7394de8e
p1nero <1070402601@qq.com>
2026-05-12 05:11:00 +0800

    用代码改战利品箱

--- src/main/java/com/p1nero/tcrcore/events/ForgeEvents.java
+++ src/main/java/com/p1nero/tcrcore/events/ForgeEvents.java
@@ -11,16 +11,26 @@ import com.p1nero.tcrcore.utils.WorldUtils;
 import com.p1nero.tcrcore.worldgen.TCRDimensions;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.IronGolem;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.merlin204.leonidas.item.LeonidasItems;
 import org.merlin204.wraithon.util.PositionTeleporter;
 
 import java.util.HashMap;
@@ -33,6 +43,51 @@ public class ForgeEvents {
     public static final Map<LivingEntity, ServerBossEvent> BOSS_BAR_MANAGER = new HashMap<>();
 
     public static final Map<ResourceKey<Level>, Queue<BlockPos>> BLOCK_POS_TO_DESTROY = new HashMap<>();
+
+    @SubscribeEvent
+    public static void onLootLoad(LootTableLoadEvent event) {
+        if (event.getName().equals(ResourceLocation.fromNamespaceAndPath("betteroceanmonuments", "chests/upper_side_chamber"))) {
+            //固定头盔
+            LootPool.Builder pool1 = LootPool.lootPool()
+                    .setRolls(ConstantValue.exactly(1))       // rolls = 1
+                    .add(LootItem.lootTableItem(LeonidasItems.THE_LOST_HELMET.get())
+                            .setWeight(1));
+
+            LootPool.Builder pool2 = LootPool.lootPool()
+                    .setRolls(ConstantValue.exactly(20))      // rolls = 20
+                    .add(LootItem.lootTableItem(Items.GOLDEN_APPLE)
+                            .setWeight(3)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 2))))
+                    .add(LootItem.lootTableItem(Items.DIAMOND)
+                            .setWeight(5)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(4, 10))))
+                    .add(LootItem.lootTableItem(Items.NAUTILUS_SHELL)
+                            .setWeight(2)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 2))))
+                    .add(LootItem.lootTableItem(Items.SPONGE)
+                            .setWeight(2)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 2))))
+                    .add(LootItem.lootTableItem(Items.PRISMARINE_SHARD)
+                            .setWeight(3)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 4))))
+                    .add(LootItem.lootTableItem(Items.PRISMARINE_CRYSTALS)
+                            .setWeight(3)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(0, 2))))
+                    .add(LootItem.lootTableItem(Items.IRON_INGOT)
+                            .setWeight(5)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(8, 24))))
+                    .add(LootItem.lootTableItem(Items.GOLD_INGOT)
+                            .setWeight(4)
+                            .apply(SetItemCountFunction.setCount(UniformGenerator.between(4, 16))));
+
+            LootTable table = LootTable.lootTable()
+                    .withPool(pool1)
+                    .withPool(pool2)
+                    .build();
+
+            event.setTable(table);
+        }
+    }
 
     @SubscribeEvent
     public static void onDialogSend(ServerNpcEntityInteractEvent event) {
