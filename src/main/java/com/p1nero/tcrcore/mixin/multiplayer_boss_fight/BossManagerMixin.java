acf3e03b43ee86d2cb5af7f085000b4031470476
p1nero <1070402601@qq.com>
2026-05-14 07:41:41 +0800

    附近玩家大于1才进行提示

--- src/main/java/com/p1nero/tcrcore/mixin/multiplayer_boss_fight/BossManagerMixin.java
+++ src/main/java/com/p1nero/tcrcore/mixin/multiplayer_boss_fight/BossManagerMixin.java
@@ -3,7 +3,7 @@ package com.p1nero.tcrcore.mixin.multiplayer_boss_fight;
 import com.p1nero.multiplayer_boss_fight.MultiplayerBossManager;
 import com.p1nero.tcrcore.TCRCoreMod;
 import net.minecraft.ChatFormatting;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,11 +12,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MultiplayerBossManager.class)
 public class BossManagerMixin {
 
-    @Inject(method = "refreshBossAttributes", at = @At(value = "INVOKE", target = "Lcom/p1nero/multiplayer_boss_fight/MultiplayerBossManager;applyAttributeModifiers(Lnet/minecraft/world/entity/LivingEntity;Lcom/p1nero/multiplayer_boss_fight/MultiplayerBossManager$ResolvedBossConfig;I)V"), remap = false)
-    private static void tcr$refreshBossAttributes(Entity target, CallbackInfo ci) {
-        target.level().players().forEach(player -> {
-            player.displayClientMessage(TCRCoreMod.getInfo("boss_health_modified_by_players").withStyle(ChatFormatting.RED), true);
-        });
+    @Inject(method = "applyAttributeModifiers", at = @At("HEAD"), remap = false)
+    private static void tcr$refreshBossAttributes(LivingEntity livingEntity, MultiplayerBossManager.ResolvedBossConfig config, int nearbyPlayers, CallbackInfo ci) {
+        if(nearbyPlayers > 1) {
+            livingEntity.level().players().forEach(player -> {
+                player.displayClientMessage(TCRCoreMod.getInfo("boss_health_modified_by_players").withStyle(ChatFormatting.RED), true);
+            });
+        }
     }
 
 }
